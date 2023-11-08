31dafcf29ec194de4c0fefc0df0d4274560fe097
Felicia Müller <felicia-muller@hotmail.com>
2023-11-08 15:19:18 +0100

    Skapat abstrakt klass Media

---
+++ Media.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtuelltBoksystem
+{
+    internal abstract class Media
+    {
+        public abstract void SpelaUpp();
+
+        public abstract void VisaInfo();
+
+    }
+}
