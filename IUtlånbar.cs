6a2e88d5c9be27eb8ef6ef365ecbc480043e22d3
Saibot155 <92667455+Saibot155@users.noreply.github.com>
2023-11-08 15:13:41 +0100

    Added IUtlånbar

---
+++ IUtlånbar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtuelltBoksystem
+{
+    internal interface IUtlånbar
+    {
+
+        public void LånaUt();
+
+        public void Återlämna();
+    }
+}
